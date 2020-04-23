0b8318d6aa02bdb2927c4cea816b9c9e122b4074
abanoubnageh91 <abanoub.nageh.adam@gmail.com>
2020-04-23 18:11:59 -0500

    fix component

--- src/app/courselist/course-list.component.spec.ts
+++ src/app/courselist/course-list.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseListComponent } from './course-list.component';
 
-describe('WelcomeComponent', () => {
+describe('CourseListComponent', () => {
   let component: CourseListComponent;
   let fixture: ComponentFixture<CourseListComponent>;
 
